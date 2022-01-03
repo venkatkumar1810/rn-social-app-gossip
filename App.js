f37b44380885416ed1934c8fda744cb6daf23eee
Ajaytummala <88574851+Ajaytummala@users.noreply.github.com>
2022-01-03 22:26:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Providers from './navigation';
 import { StatusBar } from 'expo-status-bar';
-// import StaffList from './screens/StaffList';
+// import StaffList from './screens/StaffList'
 import OnboardingScreen from './screens/OnboardingScreen';
 
 const App = () => {
